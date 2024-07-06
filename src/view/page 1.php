1bcbb3286c7e858745773a486afc756ef39cab5f
weerakoondew99 <168438180+weerakoondew99@users.noreply.github.com>
2024-07-06 21:51:15 +0530

    Register KuppiMate

---
+++ src/view/page 1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/KuppiMate/public/css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>kuppiMate</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="logodiv">
+            <img src="\KuppiMate\public\images\logo.png" >
+        </div>
+        <div class="buttondiv">
+            <span class="material-symbols-rounded" style="color: #0B5ED7 ;">arrow_back_ios
+                </span>
+
+            <div class="buttontext">
+                Back
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <div class="externalLearner card">
+            <img src="\KuppiMate\public\images\externalLearner.png">
+            <div class="text">
+                External Learner
+            </div>
+        </div>
+
+        <div class="undergraduate card">
+            <img src="\KuppiMate\public\images\undergraduate.png">
+            <div class="text">
+                Undergraduate
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
